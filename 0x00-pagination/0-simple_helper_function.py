1b1d79b6f3fd6c97dd785186e738d58bf57e41c0
robertokoba7 <okobarobert7@gmail.com>
2023-04-16 17:35:08 +0300

    [add]:returns a size two tuple that corresponds to the range of indexes

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,11 +2,8 @@
 """Simple helper function"""
 
 
-# This module provides support for type hints, including a variety of container types, such as Tuple.
 # The Tuple type represents a fixed-size, immutable sequence of elements of possibly different types.
-# It can be used to annotate the return type or parameter types of a function or method that returns or accepts a tuple.
 from typing import Tuple
-
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
