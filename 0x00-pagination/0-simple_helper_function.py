80de72dd240de4cfe027df0acbe04a42b5a7d4f5
robertokoba7 <okobarobert7@gmail.com>
2023-04-16 18:03:49 +0300

    [add]:returns a size two tuple that corresponds to the range of indexes

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,12 +2,13 @@
 """Simple helper function"""
 
 
-# The Tuple type represents a fixed-size, immutable sequence of elements of possibly different types.
+# The Tuple type represents a fixed-size, immutable sequence of elements
+# of possibly different types.
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two corresponding to the range of indexes"""
-    start = (page -1) * page_size
+    start = (page - 1) * page_size
     end = start + page_size
     return (start, end)
