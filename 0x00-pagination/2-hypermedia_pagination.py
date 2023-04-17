0a96db6a38f078a7d12f1a9a065fbd9e146a6e9f
robertokoba7 <okobarobert7@gmail.com>
2023-04-17 23:05:11 +0300

    [add]: fix

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,84 +1,75 @@
 #!/usr/bin/env python3
-"""
-server class to paginate a dataset of popular baby names.
-"""
+""" pagination project"""
 
-
+# Import necessary modules and functions
+from typing import Tuple, List, Dict, Any
 import csv
 import math
-from typing import Tuple, List, Dict, Any
 
-
-index_range = __import__("0-simple_helper_function")
+# Import helper function from another module
+index_range = __import__("0-simple_helper_function").index_range
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
     """
-    A class representing a server that provides pagination of a database.
-    """
+
+    # Define the name of the CSV file containing the dataset
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        # Initialize the dataset as None
         self.__dataset = None
 
-        def dataset(self) -> List[List]:
-            """
-            If the dataset has not been loaded yet, load it from the CSV file
-            """
-            if self.__dataset is None:
-                with open(self.DATA_FILE) as f:
-                    reader = cvs.reader(f)
-                    dataset = [row for row in reader]
-                    # Store the dataset including the header row
-                self.__dataset = dataset[1:]
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        # Load the dataset from the CSV file if it hasn't already been loaded
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
 
-            return self.__dataset
+        # Return the dataset
+        return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Return the elements of the dataset for the given page, with a pagination order.
-        """
-        # Ensure that page and page_size are positive integers
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+        """ Return the elements with a pagination order """
 
-        # Calculate the start and end indices of the page
+        # Check that the input arguments are of the correct type and value
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+
+        # Determine the start and end indices for the current page
         start, end = index_range(page, page_size)
 
-        # Initialize an empty list to hold the results
-        res_list = []
+        # Return an empty list if the start index is beyond the end of the dataset
+        if start >= len(self.dataset()):
+            return []
 
-        # If the start index is greater than or equal to the length of the
-        # dataset, return an empty list
-        if start >= len(self.dataset):
-            return res_list
-
-        # Otherwise, return the elements of the dataset for the given page
-        res_list = self.dataset
-        return res_list[start:end]
+        # Return the elements for the current page
+        return self.dataset()[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        """
-        Return a dictionary containing pagination information for the given page.
-        """
-        # Ensure that page and page_size are positive integers
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+        """ Returns a object """
+
+        # Check that the input arguments are of the correct type and value
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
 
         # Calculate the total number of pages in the dataset
-        total_pages = int(len(self.dataset) / page_size)
+        total_pages = int(len(self.dataset()) / page_size)
 
-        # Calculate the next and previous pages, if they exist
+        # Calculate the page numbers for the next and previous pages
         next_page = page + 1 if (page + 1) < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
-        # Return a dictionary containing pagination information
+        # Return an object containing pagination information and the data for the current page
         return {
             "page_size": len(self.get_page(page, page_size)),
-            "total_pages": total_pages,
             "page": page,
             "data": self.get_page(page, page_size),
             "next_page": next_page,
-            "prev_page": prev_page
+            "prev_page": prev_page,
+            "total_pages": total_pages
         }
