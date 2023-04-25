16bff05a1e187c121d68ad297831b4d243111894
robertokoba7 <okobarobert7@gmail.com>
2023-04-25 14:36:48 +0300

    [add]: basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,11 +8,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def hello():
     """
     render a html template
     """
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
