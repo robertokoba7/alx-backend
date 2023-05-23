cf6a0ca7e33ce292f9af41f0cfcc59b9b2f820da
robertokoba7 <okobarobert7@gmail.com>
2023-05-23 15:56:28 +0300

    [add]: multiple files added

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,9 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
